e6913433f3b26d197dbf99f989034ee9668491ce
Jesús López <jeslopalo@sandbox.es>
2015-12-31 00:52:30 +0100

    Add import to FunctionalInterface

--- src/main/java/es/sandbox/spikes/java8/interfaces/SimpleFunctionalInterface.java
+++ src/main/java/es/sandbox/spikes/java8/interfaces/SimpleFunctionalInterface.java
@@ -3,7 +3,7 @@ package es.sandbox.spikes.java8.interfaces;
 /**
  * Created by jeslopalo on 30/12/15.
  */
-@java.lang.FunctionalInterface
+@FunctionalInterface
 public interface SimpleFunctionalInterface {
 
     void doWork();
