321cea901bc212911aecf9b151244c60484e7280
Jesús López <jeslopalo@sandbox.es>
2015-12-30 23:48:27 +0100

    Add SimpleFunctionalInterface interface

---
+++ src/main/java/es/sandbox/spikes/java8/interfaces/SimpleFunctionalInterface.java
@@ -0,0 +1,10 @@
+package es.sandbox.spikes.java8.interfaces;
+
+/**
+ * Created by jeslopalo on 30/12/15.
+ */
+@java.lang.FunctionalInterface
+public interface SimpleFunctionalInterface {
+
+    void doWork();
+}
