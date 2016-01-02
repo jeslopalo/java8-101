cb8a4b541ebb1d5783129a00b86d22fad33d56e3
Jesús López <jeslopalo@sandbox.es>
2016-01-03 00:15:11 +0100

    Test that Functional Interfaces has only one declared method

--- src/test/java/es/sandbox/spikes/java8/interfaces/FunctionalInterfaceSpecs.java
+++ src/test/java/es/sandbox/spikes/java8/interfaces/FunctionalInterfaceSpecs.java
@@ -4,6 +4,8 @@ import es.sandbox.spikes.java8.InvocationSpy;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+
 import static es.sandbox.spikes.java8.FunctionalInterfaceCaller.call;
 import static es.sandbox.spikes.java8.InvocationSpy.spy;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,7 +23,14 @@ public class FunctionalInterfaceSpecs {
     }
 
     @Test
-    public void has_one_callable_method() {
+    public void has_only_one_declared_method() {
+        final Method[] methods = SimpleFunctionalInterface.class.getDeclaredMethods();
+
+        assertThat(methods).hasSize(1);
+    }
+
+    @Test
+    public void when_is_used_in_a_lambda_it_invokes_the_unique_declared_method() {
         call(() -> this.spy.invocation());
 
         assertThat(this.spy.invoked()).isTrue();
